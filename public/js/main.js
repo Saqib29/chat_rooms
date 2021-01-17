cab836f16b835f217385dda85bd4ea2679e8104f
Saqib <saqib29111@gmail.com>
2021-01-18 02:49:58 +0600

    add function to make object to sent to the DOM to show dynamically chat time & user

--- public/js/main.js
+++ public/js/main.js
@@ -30,8 +30,8 @@ function outputMessage(message){
     const div = document.createElement('div');
     div.classList.add('message');
 
-    div.innerHTML = `<p class="meta">Brad <span> 9:12</span></p>
-                    <p class="text">${message}</p>`;
+    div.innerHTML = `<p class="meta">${ message.username } <span>  ${ message.time }</span></p>
+                    <p class="text">${ message.text }</p>`;
 
     document.getElementById("chat-messages").appendChild(div);
 }
--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const formatMessage = require('./util/message');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+const chatBot = "ChatRoom";
 
 // Set Static folder
 app.use(express.static(path.join(__dirname, 'public')));
@@ -15,19 +17,19 @@ io.on('connection', socket => {
     console.log('New connection connected...');
 
     // give only one user who connects her/hisself
-    socket.emit('message', 'Welcome to ChatRooms');
+    socket.emit('message', formatMessage(chatBot, 'Welcome to ChatRooms'));
 
     // Broadcast when a user connects
-    socket.broadcast.emit('message', 'A new user has joined to the Chat');
+    socket.broadcast.emit('message', formatMessage(chatBot, 'A new user has joined to the Chat'));
 
     // Runs when client disconnects
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the Chat');
+        io.emit('message', formatMessage(chatBot,'A user has left the Chat'));
     });
 
     // Listen the message chatmessage from frontend side
     socket.on('chatMessage', msg => {
-        io.emit('message', msg);
+        io.emit('message', formatMessage('USER', msg));
     });
 });
 
