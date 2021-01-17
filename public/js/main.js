06dca7aa2ac25a43a95e4a38675c9d9821f08948
Saqib <saqib29111@gmail.com>
2021-01-18 02:15:51 +0600

    add chat to the DOM

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,10 @@
 const chatForm = document.getElementById('chat-form');
 const socket = io();
 
-
+// message get from server
 socket.on('message', message => {
     console.log(message);
+    outputMessage(message);
 });
 
 // Message sent
@@ -16,3 +17,14 @@ chatForm.addEventListener('submit', (e) => {
     // Emit message to the server
     socket.emit('chatMessage', msg);
 });
+
+// output message to DOM
+function outputMessage(message){
+    const div = document.createElement('div');
+    div.classList.add('message');
+
+    div.innerHTML = `<p class="meta">Brad <span> 9:12</span></p>
+                    <p class="text">${message}</p>`;
+
+    document.getElementById("chat-messages").appendChild(div);
+}
